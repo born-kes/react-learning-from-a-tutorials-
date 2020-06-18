f06a2a3e1fdcffad9ddd87dd25e1b69a23b4c72e
bornkes <born-kes@o2.pl>
2020-06-18 15:25:20 +0200

    Add repeat wave

--- src/components/Content/waveData.js
+++ src/components/Content/waveData.js
@@ -8,72 +8,54 @@ export const NoisliProvider = props => {
             name:'ogien',
             src:'ogien.ogg',
             id:'n1',
-            ref:null,
-            play:null,
             icon:'fire.svg'
         },
         {
             name:'deszcz',
             src:'deszcz.ogg',
             id:'n2',
-            ref:null,
-            play:null,
             icon:'rain.svg'
         },
         {
             name:'las',
             src:'las.ogg',
             id:'n3',
-            ref:null,
-            play:null,
             icon:'forest.svg'
         },
         {
             name:'błyskawica',
             src:'grzmot.ogg',
             id:'n4',
-            ref:null,
-            play:null,
             icon:'lightning.svg'
         },
         {
             name:'noc',
             src:'noc.ogg',
             id:'n5',
-            ref:null,
-            play:null,
             icon:'moon.svg'
         },
         {
             name:'strumyk',
             src:'strumyk.ogg',
             id:'n6',
-            ref:null,
-            play:null,
             icon:'mountain.svg'
         },
         {
             name:'szum morza',
             src:'jezioro.ogg',
             id:'n7',
-            ref:null,
-            play:null,
             icon:'wave.svg'
         },
         {
             name:'Dudnienie',
             src:'wentylator.ogg',
             id:'n8',
-            ref:null,
-            play:null,
             icon:'windmill.svg'
         },
         {
             name:'Wiatr',
             src:'wiatr.ogg',
             id:'n9',
-            ref:null,
-            play:null,
             icon:'wind.svg'
         },
 
@@ -93,24 +75,18 @@ export const MusicProvider = props => {
             name:'ElemTown.mp3',
             src:'ElemTown.mp3',
             id:'m1',
-            ref:null,
-            play:null,
             icon:''
         },
         {
             name:'Win-Scenario.mp3',
             src:'Win-Scenario.mp3',
             id:'m2',
-            ref:null,
-            play:null,
             icon:''
         },
         {
             name:'Defend-Castle.mp3',
             src:'Defend-Castle.mp3',
             id:'m3',
-            ref:null,
-            play:null,
             icon:''
         },
     ]);
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -3,6 +3,14 @@ import ReactPlayer from "react-audio-player";
 
 export class Player extends Component {
 
+    constructor(props) {
+        super(props);
+        this.play = false;
+        this.timer = 0;
+        this.delay = 500;
+        this.prevent = false;
+        this.repeat = false;
+    }
     render() {
         const prefix = this.props.prefix;
         const music = this.props.music;
@@ -15,9 +23,14 @@ export class Player extends Component {
                     src={`${path}${music.src}`}
                     title={music.name}
                     volume = {0.5}
+                    onEnded={()=>{this.ended()}}
                     ref={(el)=> {this.ref = el} }
                 />
-                <div className={`svg ${music.play}`} onClick={()=>this.start(id, music)}>
+                <div
+                    className={`svg ${this.play}`}
+                    onClick={()=>this.handleClick()}
+                    onDoubleClick={()=>this.changeRepeat() }
+                >
                     { (music.icon)? (
                         <img src={`${path}${music.icon}`} alt={music.name} title={music.name} />
                         ):(
@@ -28,13 +41,47 @@ export class Player extends Component {
         )
     }
 
-    start(id, music) {
-        music.play = !music.play;
+    start() {
+        this.play = !this.play;
 
-        if(music.play){
+        if(this.play){
             this.ref.audioEl.current.play()
+            this.playRepeat();
         }else{
             this.ref.audioEl.current.pause()
+        }
+    }
+    ended() {
+        if(this.repeat){
+            this.ref.audioEl.current.play()
+            this.playRepeat();
+        }else{
+            this.ref.audioEl.current.pause()
+        }
+    }
+    handleClick() {
+        let me = this;
+        this.timer = setTimeout(() => {
+            if (!me.prevent) {
+                me.start();
+            }
+            me.prevent = false;
+        }, me.delay);
+    }
+    changeRepeat() {
+        console.log('repeat', this.ref.audioEl.current.currentTime);
+        this.repeat = !this.repeat;
+
+        this.playRepeat();
+    }
+    playRepeat() {
+        if(this.repeat) {
+            let timeToEnd = parseInt((this.ref.audioEl.current.duration - this.ref.audioEl.current.currentTime) * 1000) - 1;
+            console.log('time to end', timeToEnd)
+
+            setTimeout(() => {
+                this.ref.audioEl.current.currentTime = 0
+            }, timeToEnd);
         }
     }
 }
