59112071b1b34105ff873060e43ab9f03b3aa333
bornkes <born-kes@o2.pl>
2020-06-26 07:30:34 +0200

    Add pop-up window Edit Player configuration
    Needed to change playback settings, to a small amount of space.

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import ReactPlayer from "react-audio-player";
 import {PlayerProgressComponent} from "./PlayerProgress";
+import Popup from "./Popup";
 
 export class Player extends Component {
 
@@ -8,39 +9,42 @@ export class Player extends Component {
         super(props);
         this.state = {
             play: false,
-            progress:1,
-            timer:0
+            progress: 1,
+            repeat: false,
+            volume: 0.5,
+            playbackRate: 2,
+            speed: 1
         }
-        this.play = false;
-        this.timer = 0;
-        this.time = 0;
-        this.delay = 500;
+        this.timer = 0; // setTimeout by doubleClick
         this.prevent = false;
-        this.repeat = false;
-        this.child = ()=>{return 'pa'};
+        this.delay = 500;
+
+        this.timerID = 0;
+
     }
     render() {
         const prefix = this.props.prefix;
         const music = this.props.music;
         const path = this.props.path;
         const id = `${prefix}-${music.id}`;
-        const style = {
-            width: `${this.state.progress}%`
-        }
         return (
             <div key={id} style={{position:"relative"}}>
                 <ReactPlayer
                     id={id}
                     src={`${path}${music.src}`}
                     title={music.name}
-                    volume = {0.5}
+                    volume = {this.state.volume}
+                    loop={this.state.repeat}
                     onEnded={()=>{this.ended()}}
+                    onPlay={this.play.bind(this)}
+                    onPause={this.pause.bind(this)}
+
                     ref={(el)=> {this.ref = el} }
                 />
                 <div
-                    className={`svg ${this.play}`}
+                    className={`svg`}
                     onClick={()=>this.handleClick()}
-                    onDoubleClick={()=>this.changeRepeat() }
+                    onDoubleClick={()=>this.changeConfig() }
                 >
                     <PlayerProgressComponent progress={this.state.progress} />
                     { (music.icon)? (
@@ -53,63 +57,85 @@ export class Player extends Component {
         )
     }
 
-    progress () {
-        if(this.play){
-            this.timerID = setInterval(
-                () => {
-                    this.setState({progress:this.state.progress + 1});
-                },
-                parseInt(this.ref.audioEl.current.duration*10)
-            );
-        }else{
-            clearInterval(this.timerID);
+    togglePlay() {
+        if(!this.state.play){
+            this.ref.audioEl.current.play();
+        }else {
+            this.ref.audioEl.current.pause();
         }
     }
 
-    start() {
-        this.play = !this.play;
+    play () {
+        const timeNow = this.ref.audioEl.current.currentTime;
+        const timeAll = this.ref.audioEl.current.duration;
+        const progress = parseInt( timeNow / timeAll * 100 )
 
-        if(this.play){
-            this.ref.audioEl.current.play()
-            this.playRepeat();
-        }else{
-            this.ref.audioEl.current.pause()
-        }
-        this.progress();
+        this.setState({play: true, progress: progress+1});
+
+        this.timerID = setInterval(() => {
+            if(this.state.progress > 100) this.state.progress = 0;
+                this.setState({progress: this.state.progress + (this.state.speed * 1)});
+            },
+            parseInt( timeAll * 10 )
+        );
     }
+
+    pause () {
+        this.setState({play:false});
+        clearInterval(this.timerID);
+    }
+
     ended() {
-        if(this.repeat){
+        if(this.state.repeat) {
+            this.ref.audioEl.current.currentTime = 0;
             this.ref.audioEl.current.play()
-            this.playRepeat();
-        }else{
-            clearInterval(this.timerID);
-            // this.ref.audioEl.current.pause()
         }
+        clearInterval(this.timerID);
         this.setState({progress:0})
     }
+
     handleClick() {
         let me = this;
-        this.timer = setTimeout(() => {
+        setTimeout(() => {
             if (!me.prevent) {
-                me.start();
+                me.togglePlay();
             }
             me.prevent = false;
         }, me.delay);
     }
-    changeRepeat() {
-        console.log('repeat', this.ref.audioEl.current.currentTime);
-        this.repeat = !this.repeat;
 
-        this.playRepeat();
+    async changeConfig() {
+        const popup =  Popup(
+            this.props,
+            {
+                play: this.togglePlay.bind(this),
+                volume: this.volume.bind(this),
+                speed: this.playbackRate.bind(this),
+                loop: this.loop.bind(this),
+                currentTime: this.currentTime.bind(this)
+            });
+        const popupResponse = await popup();
     }
-    playRepeat() {
-        if(this.repeat) {
-            const timeToEnd = parseInt((this.ref.audioEl.current.duration - this.ref.audioEl.current.currentTime) * 1000) - 1;
 
-            setTimeout(() => {
-                this.ref.audioEl.current.currentTime = 0
-                this.setState({progress:0})
-            }, timeToEnd);
-        }
+    loop( repeat ) {
+        this.setState({repeat: repeat});
+    }
+
+    volume( vol ) {
+        console.log('volume',vol)
+        this.setState({volume: parseFloat(vol) })
+    }
+
+    playbackRate( speed ) {
+        this.ref.audioEl.current.playbackRate = speed;
+        this.setState({ speed: speed});
+    }
+
+    currentTime(progress ) {
+        const timeAll = this.ref.audioEl.current.duration;
+
+        this.ref.audioEl.current.currentTime = timeAll * (progress/100);
+
+        this.setState({ progress: progress});
     }
 }
