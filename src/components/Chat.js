9b1712415f91be8377d2e3d4ec70bf5f77572311
bornkes <born-kes@o2.pl>
2020-07-07 22:19:48 +0200

    Add widget Chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,14 +1,72 @@
 // @flow
 import React, {Component} from 'react'
+import "./chat/chat.css";
+import {testDate} from "./chat/testDate";
 
 export class Chat extends Component {
+
+    constructor() {
+        super();
+
+
+        this.state = {
+            messageList: testDate()
+        };
+
+        this.refMessageList = React.createRef()
+    }
+
+    _sendMessage(event) {
+        event.preventDefault();
+        const text = event.target.firstChild.value;
+
+        if (text.length > 0) {
+            this.setState({
+                messageList: [...this.state.messageList, {
+                    id: this.state.messageList.length,
+                    author: 'my',
+                    type: 'text',
+                    data: { text:text }
+                }]
+            },
+                ()=>this.autoScrollDown())
+
+        }
+    }
+
+
     render() {
         return (
             <div id={`Chat`}>
                 <h1>Chat</h1>
-                <p>...text</p>
-                <input/>
+                <div className={`sc-message-list`} ref={this.refMessageList}>
+                {
+                    this.state.messageList.map(({author, sender, data:{text}}, i) =>(
+                        <div key={i}
+                            className={`sc-message ${i}`}
+                        >
+                            <div className={`sc-message--content ${author==='my'?`received`:`sent`}`}>
+                                <div className={`sc-message--avatar`}>img: {sender}</div>
+                                <div className={`sc-message--text`}>
+                                    <span className={`Linkify`}>{author}</span>
+                                    <div className={`sc-message--date`}>{text}</div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+                </div>
+                <div>
+                    <form  onSubmit={this._sendMessage.bind(this)}>
+                        <input/>
+                    </form>
+                </div>
             </div>
         )
+    }
+
+    autoScrollDown = () => {
+        this.refMessageList.current.lastChild
+            .scrollIntoView({block: "end", inline: "nearest", behavior: "smooth"})
     }
 }
