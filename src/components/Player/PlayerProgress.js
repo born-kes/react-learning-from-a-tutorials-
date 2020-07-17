48784e9f9ab15141749e55e2572eb27b07a6ee51
bornkes <born-kes@o2.pl>
2020-07-17 22:24:29 +0200

    remove grid on the site and transfer css by Player/playProgress.css

--- src/components/Player/PlayerProgress.js
+++ src/components/Player/PlayerProgress.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import './playProgress.css';
 
 export class PlayerProgressComponent extends Component {
     render() {
