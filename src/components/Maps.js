9072c56353ff4c2dd20b6693fb8d5b2c78c73849
bornkes <born-kes@o2.pl>
2020-07-17 10:17:00 +0200

    Add new element to maps list

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -4,13 +4,39 @@ import "react-alice-carousel/lib/alice-carousel.css";
 import {DataContext} from "./Content/Data";
 import "./maps.css"
 import {Drag} from "./Content/Drag&Drop";
+import DropZone from "react-drop-zone";
+import "./music/music2.css";
 
 export const Maps = () => {
-    const [data, /*setMaps*/] = useContext(DataContext);
+    const [data, setData] = useContext(DataContext);
+    const items =  data.Maps.map((map)=> {
+            return (
+                <div key={map.id}>
+                    <Drag class_Name={`maska`} item={map}>
+                        <img src={map.src} className="yours-custom-class" alt={map.name}/>
+                        <p>{map.name} </p>
+                    </Drag>
+                </div>
+            )
+        });
 
     return (
         <div id={`Maps`} style={{width: '210px'}}>
+            <h1>Maps
+                <DropZone accept=".jpg, .jpeg, .png, .gif, .svg" onDrop={(file) =>{
+                    const newEl = {
+                        name: file.name,
+                        src: URL.createObjectURL(file),
+                        id: `drop${Math.random()}`
+                    };
+                    console.log(newEl)
+                    setData({...data, Maps: [...data.Maps, newEl] });
+                } }>
+                    {({over}) =>over?<div className='DropZone'>Upuść tutaj</div>:<div>+</div>}
+                </DropZone>
+            </h1>
         <AliceCarousel touchTrackingEnabled={true}
+                       items={items}
                        controlsStrategy='responsive'
                        autoPlayInterval = { 2000 }
                        autoPlay = { true }
@@ -20,18 +46,7 @@ export const Maps = () => {
                            paddingLeft : 0 ,
                            paddingRight : 0
                        }}
-        >
-            {
-                data.Maps.map((map)=>(
-                    <div key={map.id}>
-                        <Drag class_Name={`maska`} item={map}>
-             <img src={map.src} className="yours-custom-class" alt={map.name} />
-                        <p>{map.name} </p>
-                        </Drag>
-                    </div>
-                ))
-            }
-        </AliceCarousel>
+        />
         </div>
     )
 }
