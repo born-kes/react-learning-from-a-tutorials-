623a8b19f9109192138b7d6b50f7952b57cbe3cc
bornkes <born-kes@o2.pl>
2020-07-17 22:03:46 +0200

    refactoring Puplit Add Pulpit.css

--- src/components/Pulpit.js
+++ src/components/Pulpit.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import DropZone from "react-drop-zone";
 import {Drop} from "./Content/Drag&Drop";
+import "./Pulpit/Pulpit.css";
 
 export class Pulpit extends Component {
     constructor(props) {
@@ -31,32 +32,14 @@ export class Pulpit extends Component {
 
 
     render() {
-        const container3d = {
-            perspective: '550px',
-            zIndex: 1,
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            right: 0
-        }
 
-        const box3d = {
-            width: '100%',
-            transform: 'rotate3d(2, 0, 0, .1turn)',
-            transformStyle: 'preserve-3d',
-            textAlign: 'center',
-        };
-        const contenderElement ={
-            maxWidth: '120vh',
-            maxHeight: '100vh',
-            width: '100%',
-            height: '100%'
-        }
+
+
 
         return (
             <div id="Pulpit">
                 <Drop
-                    style={container3d}
+                    className='container3d'
                     responseDrop={this.responseDrop}
                 >
                     <DropZone
@@ -64,8 +47,8 @@ export class Pulpit extends Component {
                     handleClick={ false }
                     >
                         { () => (
-                            <div style={box3d} className='DropZone'>
-                                <img src={this.state.src} alt={``} style={contenderElement}/>
+                            <div className='DropZone box3d'>
+                                <img src={this.state.src} alt='upuść obrazek tutaj' className='contenderElement' />
                             </div>
                             )}
                     </DropZone>
