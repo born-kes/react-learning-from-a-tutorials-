6ff464883298953d96189f8dd03ba42b8f87f586
bornkes <born-kes@o2.pl>
2020-07-20 19:44:40 +0200

    Add changeIcon and remove test data
    + the ability to change icons
    + delete randomly generated test data
    + user icons based on one file jpg

--- src/components/Content/Data.js
+++ src/components/Content/Data.js
@@ -104,7 +104,204 @@ export const DataProvider = props => {
         },
     ];
 
+    const user = ['Bot', 'Albert', 'Warrior', 'Bart', 'kes'];
+    let i=0;
+
+    const Chat = [
+        {
+            id: i++,
+            author: user[0],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Hello!!! Witam, na stronie'
+            }
+        },
+        {
+            id: i++,
+            author: user[0],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Czy jest tu ktoś ?'
+            }
+        },
+        {
+            id: i++,
+            author: user[4],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Nikogo niema, ale zostawię tu przykładową rozmowę :) żeby potomni mogli dowiedzieć się czegoś...'
+            }
+        },
+        {
+            id: i++,
+            author: user[1],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Do czego służy ta strona?'
+            }
+        },
+        {
+            id: i++,
+            author: user[2],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Bym mógł PALIĆ i Podbijać Świat :D'
+            }
+        },
+        {
+            id: i++,
+            author: user[3],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'To takie narzędzie dla Mistrzów Gry, by mogli szybko, bez przygotowania zagrać sesję. Mając pod ręką to czego potrzebują jak dźwięki, melodie i mapy'
+            }
+        },
+        {
+            id: i++,
+            author: user[2],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'To wszystko??!!'
+            }
+        },
+        {
+            id: i++,
+            author: user[3],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Wszystko? To już jest całkiem sporo na start.'
+            }
+        },
+        {
+            id: i++,
+            author: user[1],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Co mogę tu zrobić?'
+            }
+        },
+        {
+            id: i++,
+            author: user[3],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Jest to pierwszy projekt strony, taka wersja Demo... jednak kila opcji już działa'
+            }
+        },
+        {
+            id: i++,
+            author: user[3],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Na stół można położyć mapę, albo z kilku gotowych, albo z innej strony przez przeciągnij i upuść, albo z dysku.'
+            }
+        },
+        {
+            id: i++,
+            author: user[1],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Mogę położyć własną mapę na stole? Co to znaczy?'
+            }
+        },
+        {
+            id: i++,
+            author: user[3],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Na środku ekranu masz pole które nazywam stołem/pulpit możesz na niego upuścić plik graficzny i zobaczyć co się stanie'
+            }
+        },
+        {
+            id: i++,
+            author: user[1],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'No tak, można tam dać nie tylko mapę, ale też potwora, albo inny plik graficzny'
+            }
+        },
+        {
+            id: i++,
+            author: user[1],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Co to za linijka czasu na górze?'
+            }
+        },
+        {
+            id: i++,
+            author: user[3],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'To opóźniacz, pozwala uruchomić dżwięk nie od razu, ale np dopiero za 1 minute, przez co pojawia się kiedy Narrator już prowadzi opis... '
+            }
+        },
+        {
+            id: i++,
+            author: user[1],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'OK fajnie, ale jak to zrobić? i czy mogę dodać własną muzykę?'
+            }
+        },
+        {
+            id: i++,
+            author: user[3],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Tak jest przycisk dodaj dżwięk/ muzykę.'
+            }
+        },
+        {
+            id: i++,
+            author: user[3],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Na linijkę czasu kładziesz dźwięk i od tego momentu zaczyna liczyć czas, widać też jak zbliża się do końca...'
+            }
+        },
+        {
+            id: i++,
+            author: user[1],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Jak mogę zadać pytanie w przyszłości?'
+            }
+        },
+        {
+            id: i++,
+            author: user[4],
+            type: 'text',
+            sender: `ico`,
+            data: {
+                text: 'Ja zapraszam do kontaktu na Facebooku (Łukasz Mar), albo e-mail L.M.Martyn@gmail.com '
+            }
+        }
+
+    ]
+
     const [data, setData] = useState({
+        Chat: Chat,
         Noisli: Noisli,
         Music: Music,
         Maps: Maps
